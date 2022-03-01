8f578290c631a6a3fd2f1e8504460ac7922144bc
Artorias <harry.filis@yahoo.gr>
2022-03-02 00:03:04 +0200

    Create weighting filter needed to classify the best prediction subframes

---
+++ src/weighting_filter.m
@@ -0,0 +1,18 @@
+function x = weighting_filter(signal)
+   
+%% Impulse Response of the filter based on the standard
+   H = [-134, -374,0,2054,4,5741,8192,5741,2054,0,-374,-134];
+   x = zeros(1,40);
+   for k = 1:40
+       for i = 1:10
+           temp = k+5-i;
+           if temp < 0 || temp > 39
+               x(k) = 0;
+           else
+               x(k) = H(i)*signal(k+5-i);
+           end
+       end
+    
+   end
+   
+end
