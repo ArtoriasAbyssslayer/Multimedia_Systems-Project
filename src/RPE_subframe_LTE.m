6f02acd2762a34566a5da0c3f96c627d3f7d661a
Artorias <harry.filis@yahoo.gr>
2022-03-02 00:02:05 +0200

    Create the subroutine for prediction with subframe similarity

---
+++ src/RPE_subframe_LTE.m
@@ -0,0 +1,20 @@
+function [N,b] = RPE_subframe_LTE(d,PrevSubframe)
+% PrevSubFrame is the subframe from the concatenated frame
+subframe_size = 40;
+    %% similarity testing of the previous subframes
+    % index = size(PrevSubframe)+39-lambda;
+    for lambda = subframe_size:120
+        index = size(PrevSubframe)-lambda+1;
+        R(lambda-subframe_size+1) = sum(d.*PrevSubframe(index:index+39));
+    end
+    [R_max,maximizer_index] = max(R);
+    N = maximizer_index + 39;   
+
+    %beta estimation - amplitude difference
+    %prev_frame that matches
+    Prev_Match =  PrevSubframe(size(PrevSubframe)-N+1:size(PrevSubframe)-N+40);
+    prevd_autocorr = sum(Prev_Match.*Prev_Match);
+    b = R_max/prevd_autocorr;
+
+end
+
